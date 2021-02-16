e589a43905684eb96d069fd131c3d7a4e4b24245
Arseniy Kononov <kononovarseniy@gmail.com>
2021-02-16 15:11:38 +0700

    Add constructors/destructors/operators for ShaderProgram and PositionedObject

--- src/Kononov/Task1/FirstShader.cpp
+++ src/Kononov/Task1/FirstShader.cpp
@@ -6,79 +6,79 @@ FirstShader::FirstShader() {
   /*
    * Shader program initialization
    */
-  const auto &shader = getShader();
+  auto &shader = getShader();
 
   const char *const vertex_shader = ":/shaders/first.vs";
   const char *const fragment_shader = ":/shaders/first.fs";
-  shader->addShaderFromSourceFile(QOpenGLShader::Vertex, vertex_shader);
-  shader->addShaderFromSourceFile(QOpenGLShader::Fragment, fragment_shader);
-  shader->link();
+  shader.addShaderFromSourceFile(QOpenGLShader::Vertex, vertex_shader);
+  shader.addShaderFromSourceFile(QOpenGLShader::Fragment, fragment_shader);
+  shader.link();
 
   /*
    * Query locations of parameters
    */
-  m_vertex_position_attr = shader->attributeLocation("vertex_position");
-  m_vertex_normal_attr = shader->attributeLocation("vertex_normal");
-  m_vertex_uv_attr = shader->attributeLocation("vertex_uv");
+  m_vertex_position_attr = shader.attributeLocation("vertex_position");
+  m_vertex_normal_attr = shader.attributeLocation("vertex_normal");
+  m_vertex_uv_attr = shader.attributeLocation("vertex_uv");
 
-  m_model_view_matrix_uniform = shader->uniformLocation("model_view_matrix");
-  m_model_matrix_uniform = shader->uniformLocation("model_matrix");
-  m_normal_matrix_uniform = shader->uniformLocation("normal_matrix");
+  m_model_view_matrix_uniform = shader.uniformLocation("model_view_matrix");
+  m_model_matrix_uniform = shader.uniformLocation("model_matrix");
+  m_normal_matrix_uniform = shader.uniformLocation("normal_matrix");
 
-  m_diffuse_map_uniform = shader->uniformLocation("diffuse_map");
-  m_ambient_strength_uniform = shader->uniformLocation("ambient_strength");
-  m_specular_strength_uniform = shader->uniformLocation("specular_strength");
-  m_specular_pow_uniform = shader->uniformLocation("specular_pow");
-  m_light_color_uniform = shader->uniformLocation("light_color");
-  m_light_pos_uniform = shader->uniformLocation("light_pos");
-  m_view_pos_uniform = shader->uniformLocation("view_pos");
+  m_diffuse_map_uniform = shader.uniformLocation("diffuse_map");
+  m_ambient_strength_uniform = shader.uniformLocation("ambient_strength");
+  m_specular_strength_uniform = shader.uniformLocation("specular_strength");
+  m_specular_pow_uniform = shader.uniformLocation("specular_pow");
+  m_light_color_uniform = shader.uniformLocation("light_color");
+  m_light_pos_uniform = shader.uniformLocation("light_pos");
+  m_view_pos_uniform = shader.uniformLocation("view_pos");
 }
 
 void FirstShader::enableAttributeArrays() {
-  getShader()->enableAttributeArray(m_vertex_position_attr);
-  getShader()->enableAttributeArray(m_vertex_uv_attr);
-  getShader()->enableAttributeArray(m_vertex_normal_attr);
+  getShader().enableAttributeArray(m_vertex_position_attr);
+  getShader().enableAttributeArray(m_vertex_uv_attr);
+  getShader().enableAttributeArray(m_vertex_normal_attr);
 }
 
 void FirstShader::disableAttributeArrays() {
-  getShader()->disableAttributeArray(m_vertex_position_attr);
-  getShader()->disableAttributeArray(m_vertex_uv_attr);
-  getShader()->disableAttributeArray(m_vertex_normal_attr);
+  getShader().disableAttributeArray(m_vertex_position_attr);
+  getShader().disableAttributeArray(m_vertex_uv_attr);
+  getShader().disableAttributeArray(m_vertex_normal_attr);
 }
 
 void FirstShader::setVertexPositionBuffer(int offset, int stride) {
-  getShader()->setAttributeBuffer(m_vertex_position_attr, GL_FLOAT, offset, 3,
+  getShader().setAttributeBuffer(m_vertex_position_attr, GL_FLOAT, offset, 3,
                                   stride);
 }
 
 void FirstShader::setVertexNormalBuffer(int offset, int stride) {
-  getShader()->setAttributeBuffer(m_vertex_normal_attr, GL_FLOAT, offset, 3,
+  getShader().setAttributeBuffer(m_vertex_normal_attr, GL_FLOAT, offset, 3,
                                   stride);
 }
 
 void FirstShader::setVertexUVBuffer(int offset, int stride) {
-  getShader()->setAttributeBuffer(m_vertex_uv_attr, GL_FLOAT, offset, 2,
+  getShader().setAttributeBuffer(m_vertex_uv_attr, GL_FLOAT, offset, 2,
                                   stride);
 }
 
 void FirstShader::setMatrices(QMatrix4x4 view, QMatrix4x4 model) {
-  getShader()->setUniformValue(FirstShader::m_model_view_matrix_uniform,
+  getShader().setUniformValue(FirstShader::m_model_view_matrix_uniform,
                                view * model);
-  getShader()->setUniformValue(FirstShader::m_model_matrix_uniform, model);
-  getShader()->setUniformValue(FirstShader::m_normal_matrix_uniform,
+  getShader().setUniformValue(FirstShader::m_model_matrix_uniform, model);
+  getShader().setUniformValue(FirstShader::m_normal_matrix_uniform,
                                model.normalMatrix());
 }
 
 void FirstShader::setParameters(FirstShaderParameters params) {
-  getShader()->setUniformValue(m_diffuse_map_uniform, params.getDiffuseMap());
-  getShader()->setUniformValue(m_ambient_strength_uniform,
+  getShader().setUniformValue(m_diffuse_map_uniform, params.getDiffuseMap());
+  getShader().setUniformValue(m_ambient_strength_uniform,
                                params.getAmbientStrength());
-  getShader()->setUniformValue(m_specular_strength_uniform,
+  getShader().setUniformValue(m_specular_strength_uniform,
                                params.getSpecularStrength());
-  getShader()->setUniformValue(m_specular_pow_uniform, params.getSpecularPow());
-  getShader()->setUniformValue(m_light_pos_uniform, params.getLightPos());
-  getShader()->setUniformValue(m_light_color_uniform, params.getLightColor());
-  getShader()->setUniformValue(m_view_pos_uniform, params.getViewPos());
+  getShader().setUniformValue(m_specular_pow_uniform, params.getSpecularPow());
+  getShader().setUniformValue(m_light_pos_uniform, params.getLightPos());
+  getShader().setUniformValue(m_light_color_uniform, params.getLightColor());
+  getShader().setUniformValue(m_view_pos_uniform, params.getViewPos());
 }
 
 void FirstShaderParameters::setAmbient(GLfloat strength) {
