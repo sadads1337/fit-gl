cc0057017adcd6d1eec25aa9c0d054ec657be818
lexxsage <alexdevyatovskaya@gmail.com>
2021-02-26 15:57:41 +0700

    finaly

--- src/devyatovskaya/Task1/cubewindow.h
+++ src/devyatovskaya/Task1/cubewindow.h
@@ -21,37 +21,36 @@ public:
             cube_geometry_.InitGeometry();
             edges_geometry_.InitGeometry();
         }
-    void mousePressEvent(QMouseEvent* e) override;
-    void mouseReleaseEvent(QMouseEvent* e) override;
+  void mousePressEvent(QMouseEvent* e) override;
+  void mouseReleaseEvent(QMouseEvent* e) override;
 
   void init() override;
   void render() override;
 
 private:
   QColorDialog color_dialog_;
-     QMatrix4x4 projection_{};
+  QMatrix4x4 projection_{};
 
-      QSlider slider_x_;
-      QSlider slider_y_;
-      QSlider slider_z_;
+  QSlider slider_x_;
+  QSlider slider_y_;
+  QSlider slider_z_;
 
-      CubeGeometry cube_geometry_{ 1.0f };
-      CubeEdges edges_geometry_{ 1.0f };
-      CubePainter painter_{ cube_geometry_ };
+  CubeGeometry cube_geometry_{ 1.0f };
+  CubeEdges edges_geometry_{ 1.0f };
+  CubePainter painter_{ cube_geometry_ };
 
-      QVector2D mouse_press_position_;
-        QVector3D rotation_axis_;
-        qreal angular_speed_ = 0;
-        QQuaternion rotation_;
+  QVector2D mouse_press_position_;
+  QVector3D rotation_axis_;
+  qreal angular_speed_ = 0;
+  QQuaternion rotation_;
 
-        void setRotation();
-           void initColorDialog();
-            void initSlidersDialog();
-            void resizeGL();
+  void setRotation();
+  void initColorDialog();
+  void initSlidersDialog();
+  void resizeGL();
 
-    //std::unique_ptr<QOpenGLShaderProgram> program_ = nullptr;
-    QOpenGLShaderProgram program_{ this };
-     int frame_ = 0;
+  QOpenGLShaderProgram program_{ this };
+  int frame_ = 0;
 };
 
 } // namespace fgl
