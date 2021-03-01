cead77e1c11c0af16bd477f1f98e1b16bce24a35
gergler <sayfudinovap@gmail.com>
2021-03-01 18:29:37 +0700

    Task1 working

--- src/sayfudinova/task1/CubeWindow.h
+++ src/sayfudinova/task1/CubeWindow.h
@@ -2,46 +2,43 @@
 #define CUBE_CUBEWINDOW_H
 
 #include <Base/GLWindow.hpp>
-#include <memory>
-#include <QOpenGLShaderProgram>
 #include <QColor>
 #include <QColorDialog>
 #include <QKeyEvent>
 #include <QMatrix4x4>
+#include <QOpenGLShaderProgram>
 #include <QQuaternion>
 #include <QVector2D>
 #include <QVector3D>
+#include <memory>
 
 namespace fgl {
 
-    class CubeWindow final : public GLWindow {
-        Q_OBJECT
-    public:
-        void init() override;
-        void render() override;
+  class CubeWindow final : public GLWindow {
 
-    protected:
-        void mousePressEvent(QMouseEvent *e) override;
-        void mouseReleaseEvent(QMouseEvent *e) override;
-        void keyPressEvent(QKeyEvent *e) override;
+  public:
+    void init() override;
+    void render() override;
 
-    private:
-        // Attributes and uniforms handlers.
-        GLint posAttr_ = 0;
-        GLint colAttr_ = 0;
-        GLint matrixUniform_ = 0;
+  protected:
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
+    void keyPressEvent(QKeyEvent *event) override;
 
-        // Shader program handler.
-        std::unique_ptr<QOpenGLShaderProgram> program_ = nullptr;
+  private:
+    GLint posAttr_ = 0;
+    GLint matrixUniform_ = 0;
 
-        // Frame counter for animation.
-        int frame_ = 0;
+    std::unique_ptr<QOpenGLShaderProgram> program_ = nullptr;
 
-        QVector4D changeColor {1.0, 1.0,   1.0, 1};
-        QVector2D mousePressPosition;
-        QVector3D rotationAxis;
-        qreal angularSpeed = 0;
-    };
+    QVector4D changeColor{0.0, 1.0, 1.0, 1};
+
+    int frame_ = 0;
+
+    QVector2D mousePressPosition{0., 0.};
+    QVector3D rotationAxis{0., 0., 1.};
+  };
 
 } // namespace fgl
-#endif //CUBE_CUBEWINDOW_H
+
+#endif // CUBE_CUBEWINDOW_H
