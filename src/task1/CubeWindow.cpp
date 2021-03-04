fc1b76c09a2b54239c2a56c73612a5742ad568ce
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2021-03-04 18:11:00 +0700

    Update CubeWindow.cpp

--- src/task1/CubeWindow.cpp
+++ src/task1/CubeWindow.cpp
@@ -55,7 +55,9 @@ namespace fgl {
         assert(colAttr_ != -1);
         matrixUniform_ = program_->uniformLocation("matrix");
         assert(matrixUniform_ != -1);
-
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_CULL_FACE);
 
