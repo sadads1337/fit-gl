45039002bc34de3f4e759f33c3231d451288cb60
gergler <sayfudinovap@gmail.com>
2021-03-04 16:00:45 +0700

    Task1

---
+++ src/sayfudinova/task1/CubeWindow.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Base/GLWindow.hpp>
+#include <QColor>
+#include <QColorDialog>
+#include <QKeyEvent>
+#include <QMatrix4x4>
+#include <QOpenGLShaderProgram>
+#include <QQuaternion>
+#include <QVector2D>
+#include <QVector3D>
+#include <memory>
+
+class CubeWindow final : public fgl::GLWindow {
+
+public:
+  void init() override;
+  void render() override;
+
+protected:
+  void mousePressEvent(QMouseEvent *e) override;
+  void mouseReleaseEvent(QMouseEvent *e) override;
+  void keyPressEvent(QKeyEvent *event) override;
+
+private:
+  GLint posAttr_ = 0;
+  GLint matrixUniform_ = 0;
+
+  std::unique_ptr<QOpenGLShaderProgram> program_ = nullptr;
+
+  QVector4D changeColor{0.0, 1.0, 1.0, 1};
+
+  int frame_ = 0;
+
+  QVector2D mousePressPosition{0., 0.};
+  QVector3D rotationAxis{0., 0., 1.};
+};
