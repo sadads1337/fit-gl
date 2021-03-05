ce898a4e91f9a4f7a3f0b6e9d7426f0a1049708e
lexxsage <alexdevyatovskaya@gmail.com>
2021-03-05 16:13:55 +0700

    try to fix build

--- src/devyatovskaya/Task2/GLFlatMeshGenerator.h
+++ src/devyatovskaya/Task2/GLFlatMeshGenerator.h
@@ -12,7 +12,7 @@ public:
 private:
 	float edge_len_;
 	void init_vertices(std::vector<GLVertex>& vertices, std::size_t mesh_steps) const;
-	void init_indices(std::vector<unsigned>& indices, std::size_t mesh_steps) const;
+	void init_indices(std::vector<unsigned>& indices, unsigned mesh_steps);
 
 	GLMesh generate() const override;
 };
