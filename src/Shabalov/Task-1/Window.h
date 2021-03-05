88e5e3becd29a0b69b63c07bc4da6fd99d28a038
Ilya Makarov <i.o.makarov@2gis.ru>
2021-03-05 13:09:46 +0300

    Format code with clang-format

--- src/Shabalov/Task-1/Window.h
+++ src/Shabalov/Task-1/Window.h
@@ -1,42 +1,31 @@
-//
-// Created by nikita on 22.02.2021.
-//
+#pragma once
 
-#ifndef TASK_1_WINDOW_H
-#define TASK_1_WINDOW_H
-
-
-#include <QWidget>
-#include <QSlider>
-#include <QLabel>
-#include <QColorDialog>
-#include <QVBoxLayout>
-#include <memory>
 #include "Widget.h"
+#include <QColorDialog>
+#include <QLabel>
+#include <QSlider>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <memory>
 
-class Window : public QWidget
-{
-Q_OBJECT
+class Window : public QWidget {
+  Q_OBJECT
 
 public:
-    Window();
+  Window();
 
 private:
-    static std::unique_ptr<QSlider> createSlider();
+  static std::unique_ptr<QSlider> createSlider();
 
-    std::unique_ptr<Widget> widget;
-    std::unique_ptr<QSlider> xSlider;
-    std::unique_ptr<QSlider> ySlider;
-    std::unique_ptr<QSlider> zSlider;
-    std::unique_ptr<QLabel> xlabel;
-    std::unique_ptr<QLabel> ylabel;
-    std::unique_ptr<QLabel> zlabel;
-    std::unique_ptr<QColorDialog> color;
-    std::unique_ptr<QVBoxLayout> mainLayout;
-    std::unique_ptr<QGridLayout> container;
-    std::unique_ptr<QWidget> w;
+  std::unique_ptr<Widget> widget;
+  std::unique_ptr<QSlider> xSlider;
+  std::unique_ptr<QSlider> ySlider;
+  std::unique_ptr<QSlider> zSlider;
+  std::unique_ptr<QLabel> xlabel;
+  std::unique_ptr<QLabel> ylabel;
+  std::unique_ptr<QLabel> zlabel;
+  std::unique_ptr<QColorDialog> color;
+  std::unique_ptr<QVBoxLayout> mainLayout;
+  std::unique_ptr<QGridLayout> container;
+  std::unique_ptr<QWidget> w;
 };
-
-
-
-#endif //TASK_1_WINDOW_H
--- src/Shabalov/Task-1/main.cpp
+++ src/Shabalov/Task-1/main.cpp
@@ -1,20 +1,20 @@
-#include <QApplication>
-#include <QSurfaceFormat>
-#include <QDesktopWidget>
-#include "Widget.h"
 #include "MainWindow.h"
+#include "Widget.h"
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QSurfaceFormat format;
-    format.setSamples(16);
-    format.setDepthBufferSize(24);
-    QSurfaceFormat::setDefaultFormat(format);
+  QApplication app(argc, argv);
+  QSurfaceFormat format;
+  format.setSamples(16);
+  format.setDepthBufferSize(24);
+  QSurfaceFormat::setDefaultFormat(format);
 
-    app.setApplicationName("Cube");
+  app.setApplicationName("Cube");
 
-    MainWindow mainWindow;
-    mainWindow.showMaximized();
+  MainWindow mainWindow;
+  mainWindow.showMaximized();
 
-    return app.exec();
+  return app.exec();
 }
