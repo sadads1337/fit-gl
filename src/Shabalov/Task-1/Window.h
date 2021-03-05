038127c5b8b5a89701154a6e98c58fee56318342
Ilya Makarov <i.o.makarov@2gis.ru>
2021-03-05 13:49:58 +0300

    Remove memory leak (double free)

--- src/Shabalov/Task-1/Window.h
+++ src/Shabalov/Task-1/Window.h
@@ -13,19 +13,4 @@ class Window : public QWidget {
 
 public:
   Window();
-
-private:
-  static std::unique_ptr<QSlider> createSlider();
-
-  std::unique_ptr<Widget> widget;
-  std::unique_ptr<QSlider> xSlider;
-  std::unique_ptr<QSlider> ySlider;
-  std::unique_ptr<QSlider> zSlider;
-  std::unique_ptr<QLabel> xlabel;
-  std::unique_ptr<QLabel> ylabel;
-  std::unique_ptr<QLabel> zlabel;
-  std::unique_ptr<QColorDialog> color;
-  std::unique_ptr<QVBoxLayout> mainLayout;
-  std::unique_ptr<QGridLayout> container;
-  std::unique_ptr<QWidget> w;
 };
