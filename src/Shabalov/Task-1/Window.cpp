99c2166a79add99d0df6429c09c963c162538afa
Ilya Makarov <i.o.makarov@2gis.ru>
2021-03-05 13:49:58 +0300

    Remove memory leak (double free)

--- src/Shabalov/Task-1/Window.cpp
+++ src/Shabalov/Task-1/Window.cpp
@@ -1,52 +1,68 @@
 #include "Window.h"
 #include "Widget.h"
 
-Window::Window() {
-  widget = std::make_unique<Widget>();
+namespace {
 
-  xSlider = createSlider();
-  ySlider = createSlider();
-  zSlider = createSlider();
-  xlabel = std::make_unique<QLabel>("X");
-  ylabel = std::make_unique<QLabel>("Y");
-  zlabel = std::make_unique<QLabel>("Z");
-  color = std::make_unique<QColorDialog>();
+template <typename T, typename = std::enable_if_t<std::is_pointer<T>::value>>
+using owner = T;
 
-  connect(xSlider.get(), &QSlider::valueChanged, widget.get(),
-          &Widget::setXRotation);
-  connect(ySlider.get(), &QSlider::valueChanged, widget.get(),
-          &Widget::setYRotation);
-  connect(zSlider.get(), &QSlider::valueChanged, widget.get(),
-          &Widget::setZRotation);
-  connect(color.get(), &QColorDialog::currentColorChanged, widget.get(),
-          &Widget::setColor);
-
-  mainLayout = std::make_unique<QVBoxLayout>();
-  container = std::make_unique<QGridLayout>();
-  container->addWidget(widget.get(), 0, 0, 2, 1);
-  container->addWidget(xSlider.get(), 1, 1, 1, 1);
-  container->addWidget(ySlider.get(), 1, 2, 1, 1);
-  container->addWidget(zSlider.get(), 1, 3, 1, 1);
-  container->addWidget(xlabel.get(), 0, 1, 1, 1);
-  container->addWidget(ylabel.get(), 0, 2, 1, 1);
-  container->addWidget(zlabel.get(), 0, 3, 1, 1);
-  container->addWidget(color.get(), 2, 0, 1, 3);
-
-  w = std::make_unique<QWidget>();
-  w->setLayout(container.get());
-  mainLayout->addWidget(w.get());
-  setLayout(mainLayout.get());
-
-  xSlider->setValue(50);
-  ySlider->setValue(50);
-  zSlider->setValue(50);
-  color->setCurrentColor(QColor(Qt::gray));
-}
-
-std::unique_ptr<QSlider> Window::createSlider() {
-  auto slider = std::make_unique<QSlider>(Qt::Vertical);
-  slider->setRange(0, 100);
-  slider->setSingleStep(10);
+[[nodiscard]] auto createSlider() {
+  constexpr auto min = 0;
+  constexpr auto max = 100;
+  constexpr auto tick = 10;
+  auto * const slider = owner<QSlider *>( new QSlider(Qt::Vertical));
+  slider->setRange(min, max);
+  slider->setSingleStep(tick);
   slider->setTickPosition(QSlider::TicksRight);
   return slider;
+}
+
+} // namespace
+
+Window::Window() {
+  auto * const widget = owner<Widget *>(new Widget);
+  
+  auto * const xSlider = createSlider();
+  auto * const ySlider = createSlider();
+  auto * const zSlider = createSlider();
+
+  auto * const color = owner<QColorDialog *>(new QColorDialog);
+
+  auto * const container = owner<QGridLayout *>(new QGridLayout);
+
+  container->addWidget(widget, 0, 0, 2, 1);
+
+  container->addWidget(xSlider, 1, 1, 1, 1);
+  container->addWidget(ySlider, 1, 2, 1, 1);
+  container->addWidget(zSlider, 1, 3, 1, 1);
+
+  container->addWidget(color, 2, 0, 1, 3);
+
+  container->addWidget(new QLabel("X"), 0, 1, 1, 1);
+  container->addWidget(new QLabel("Y"), 0, 2, 1, 1);
+  container->addWidget(new QLabel("Z"), 0, 3, 1, 1);
+
+  auto * const mainLayout = owner<QVBoxLayout *>(new QVBoxLayout);
+
+  auto * const w = owner<QWidget *>(new QWidget);
+  w->setLayout(container);
+  mainLayout->addWidget(w);
+  setLayout(mainLayout);
+
+  connect(xSlider, &QSlider::valueChanged, widget,
+          &Widget::setXRotation);
+  connect(ySlider, &QSlider::valueChanged, widget,
+          &Widget::setYRotation);
+  connect(zSlider, &QSlider::valueChanged, widget,
+          &Widget::setZRotation);
+  connect(color, &QColorDialog::currentColorChanged,
+          widget, &Widget::setColor);
+
+  constexpr auto defaultValue = 50;
+
+  xSlider->setValue(defaultValue);
+  ySlider->setValue(defaultValue);
+  zSlider->setValue(defaultValue);
+
+  color->setCurrentColor(QColor(Qt::gray));
 }
