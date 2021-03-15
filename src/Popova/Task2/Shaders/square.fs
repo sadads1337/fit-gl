4d6f1ee42eee4b8ecd791e8d52c45eb47af5e0cf
Алина Попова <alina99popova@gmail.com>
2021-03-15 21:50:38 +0700

    task2

--- src/Popova/Task2/Shaders/square.fs
+++ src/Popova/Task2/Shaders/square.fs
@@ -1,7 +1,12 @@
 #version 120
 
 uniform vec4 col = vec4(1.0, 0.0, 0.0, 1.0);
-
+uniform int flag = 0;
 void main() {
-   gl_FragColor = col;
+   if (flag == 1){
+      gl_FragColor = col;
+   } else{
+      gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+   }
+   
 }
