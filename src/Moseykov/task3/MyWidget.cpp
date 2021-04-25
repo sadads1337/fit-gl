392677c55cbe89e63df34519ab9a92ec28a19147
Ilya Makarov <i.o.makarov@2gis.ru>
2021-04-25 12:59:03 +0700

    Code style

--- src/Moseykov/task3/MyWidget.cpp
+++ src/Moseykov/task3/MyWidget.cpp
@@ -2,10 +2,10 @@
 
 namespace {
 
-template<typename T, typename = std::enable_if_t<std::is_pointer<T>::value>>
+template <typename T, typename = std::enable_if_t<std::is_pointer<T>::value>>
 using main = T;
 
-auto morph_slider(int min_value,int max_value, int tick_value) {
+auto morph_slider(int min_value, int max_value, int tick_value) {
   auto *const slider = main<QSlider *>(new QSlider(Qt::Horizontal));
   slider->setRange(min_value, max_value);
   slider->setSingleStep(tick_value);
@@ -13,20 +13,20 @@ auto morph_slider(int min_value,int max_value, int tick_value) {
   slider->setValue(max_value);
   return slider;
 }
-}
+} // namespace
 
-MyWidget::MyWidget()
-{
-  auto * const my_window = main<fgl::SquareWindow *>(new fgl::SquareWindow);
-  auto * const tSlider = morph_slider(0, 1000, 50);
+MyWidget::MyWidget() {
+  auto *const my_window = main<fgl::SquareWindow *>(new fgl::SquareWindow);
+  auto *const tSlider = morph_slider(0, 1000, 50);
 
-  auto * const container = main<QGridLayout *>(new QGridLayout);
-  auto * const mainLayout = main<QVBoxLayout *>(new QVBoxLayout);
-  auto * const w = main<QWidget *>(new QWidget);
+  auto *const container = main<QGridLayout *>(new QGridLayout);
+  auto *const mainLayout = main<QVBoxLayout *>(new QVBoxLayout);
+  auto *const w = main<QWidget *>(new QWidget);
   container->addWidget(my_window);
   container->addWidget(tSlider);
   w->setLayout(container);
   mainLayout->addWidget(w);
   setLayout(mainLayout);
-  connect(tSlider, &QSlider::valueChanged, my_window, &fgl::SquareWindow::change_morph_param);
+  connect(tSlider, &QSlider::valueChanged, my_window,
+          &fgl::SquareWindow::change_morph_param);
 }
