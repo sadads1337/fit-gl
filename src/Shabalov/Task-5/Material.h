8e09110abbca4d8e401981fd61f206155bdc4e02
Nikita Shabalov <41150488+nickshabalov@users.noreply.github.com>
2021-05-03 14:13:26 +0700

    Update Material.h

--- src/Shabalov/Task-5/Material.h
+++ src/Shabalov/Task-5/Material.h
@@ -4,17 +4,17 @@
 #include <utility>
 
 struct Material{
-    Material(const QVector3D &dC, const float sF, const float  rI, const QVector4D &p) : diffuseColor(dC), specularFactor(sF), refractiveIndex(rI), proportion(p), img(nullptr){}
+    Material(const QVector3D &dC, const float sF, const float  rI, const QVector4D &p) : diffuseColor(dC), img(nullptr), specularFactor(sF), refractiveIndex(rI), proportion(p){}
     explicit Material(QImage i) : img(std::move(i)), refractiveIndex(1), proportion(1., 0., 0., 0.){}
-    Material() : refractiveIndex(1), proportion(1., 0., 0., 0.), img(nullptr), diffuseColor(), specularFactor(){}
+    Material() : img(nullptr), refractiveIndex(1), proportion(1., 0., 0., 0.), diffuseColor(), specularFactor(){}
     [[nodiscard]] bool isTexture() const {
         if(img.isNull()) return false;
         return true;
     }
     QVector3D diffuseColor;
+    QImage img;
     float specularFactor{};
     float refractiveIndex;
     QVector4D proportion;
-    QImage img;
 
 };
