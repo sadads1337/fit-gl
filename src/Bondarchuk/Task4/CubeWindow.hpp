7b8b48258e2c566e752c397c553bb5f1f1cfaf0a
wbondarchuk <55180931+wbondarchuk@users.noreply.github.com>
2021-05-17 15:53:45 +0700

    Added task 4

---
+++ src/Bondarchuk/Task4/CubeWindow.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <Base/GLWindow.hpp>
+
+#include <memory>
+
+#include <QOpenGLTexture>
+#include <QOpenGLBuffer>
+#include <QOpenGLShaderProgram>
+#include <QColor>
+#include <QColorDialog>
+#include <QKeyEvent>
+#include <QOpenGLBuffer>
+#include <QVector2D>
+#include <QVector3D>
+#include <QMatrix4x4>
+#include <QQuaternion>
+
+
+
+namespace fgl {
+
+class CubeWindow final : public GLWindow {
+
+public:
+  void init() override;
+  void render() override;
+
+
+protected:
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseReleaseEvent(QMouseEvent *event) override;
+  void keyPressEvent(QKeyEvent *event) override;
+  void initTextures();
+  void initTangentBitangent();
+
+private:
+  // Attributes and uniforms handlers.
+  GLint posAttr_ = 0;
+  GLint colAttr_ = 0;
+  GLint norm_ = 0;
+  GLint tangent_ = 0;
+  GLint bitangent_ = 0;
+  GLint textCoord_ = 0;
+  GLint matrixUniform_ = 0;
+  GLint matrixUniform2_ = 0;
+  QOpenGLTexture *texture = nullptr;
+  QOpenGLTexture *normal_map = nullptr;
+
+  // Shader program handler.
+  std::unique_ptr<QOpenGLShaderProgram> program_ = nullptr;
+
+  // Frame counter for animation.
+  int frame_ = 0;
+
+  QOpenGLBuffer vbo;
+  QOpenGLBuffer ibo{QOpenGLBuffer::IndexBuffer};
+
+  QVector4D cube_color{1.0, 1.0, 1.0, 1};
+  QVector2D mousePressPosition{0.0, 0.0};
+  QVector3D rotationAxis = QVector3D(0.0, 1.0, 0.0).normalized();
+  
+};
+
+} // namespace fgl
