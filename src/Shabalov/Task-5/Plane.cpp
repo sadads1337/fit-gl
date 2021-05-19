dacf7021bd759777fc31de1212b3430e7c2575ef
Nikita Shabalov <41150488+nickshabalov@users.noreply.github.com>
2021-05-19 21:34:48 +0700

    Add constant, rename variable, Use auto const

--- src/Shabalov/Task-5/Plane.cpp
+++ src/Shabalov/Task-5/Plane.cpp
@@ -1,15 +1,17 @@
 #include "Plane.h"
 #include <utility>
 
+constexpr auto precision = 1e-6;
+
 Plane::Plane(const QVector3D &p, const QVector3D &n, Material m) : position(p), normal(n), material(std::move(m)){
 }
 
-bool Plane::ray_intersect(const Ray &ray, float &t) const {
-    auto denom = QVector3D::dotProduct(-normal, ray.direction);
-    if(std::abs(denom) > 1e-6){
+bool Plane::ray_intersect(const Ray &ray, float &distance) const {
+    const auto denom = QVector3D::dotProduct(-normal, ray.direction);
+    if(std::abs(denom) > precision){
         QVector3D dist = position - ray.origin;
-        t = QVector3D::dotProduct(dist, -normal) / denom;
-        return (t >= 0);
+        distance = QVector3D::dotProduct(dist, -normal) / denom;
+        return (distance >= 0.f);
     }
     return false;
 }
