5cd7b686a2df92494be5035b544abaaceda39542
Nikita Shabalov <41150488+nickshabalov@users.noreply.github.com>
2021-05-19 21:38:18 +0700

    Rename variables, use auto const in sphere.cpp

--- src/Shabalov/Task-5/Sphere.cpp
+++ src/Shabalov/Task-5/Sphere.cpp
@@ -6,19 +6,19 @@
 Sphere::Sphere(const QVector3D &c,const float r, Material m)
 : center(c), radius(r), material(std::move(m)){}
 
-bool Sphere::ray_intersect(const Ray &ray, float &t) const {
-    QVector3D L =  center - ray.origin;
-    auto tca = QVector3D::dotProduct(L,ray.direction);
-    auto d2 = QVector3D::dotProduct(L,L) - tca*tca;
+bool Sphere::ray_intersect(const Ray &ray, float &distance) const {
+    const auto L =  center - ray.origin;
+    const auto tca = QVector3D::dotProduct(L,ray.direction);
+    const auto d2 = QVector3D::dotProduct(L,L) - tca*tca;
     if(d2 > radius * radius) return false;
-    auto thc = sqrt(radius * radius - d2);
+    const auto thc = sqrt(radius * radius - d2);
     auto t0 = tca - thc;
-    auto t1 = tca + thc;
+    const auto t1 = tca + thc;
 
     if(t0 < 0.0){
         t0 = t1;
         if(t0 < 0.0) return false;
     }
-    t = t0;
+    distance = t0;
     return true;
 }
