781bf693f93a009646d9bdf2374903de594f07d2
Nikita Shabalov <41150488+nickshabalov@users.noreply.github.com>
2021-05-19 21:40:33 +0700

    Fix main.c , use constants, add const auto, delete constructor for ray&light,

--- src/Shabalov/Task-5/main.cpp
+++ src/Shabalov/Task-5/main.cpp
@@ -9,37 +9,41 @@
 #include "Plane.h"
 #include <memory>
 
-#ifndef M_PI
-    #define M_PI 3.14159265358979323846
-#endif
+
 
 constexpr auto WIDTH = 1280;
 constexpr auto HEIGHT = 960;
-constexpr auto FOV = M_PI/3.;
+constexpr auto PI = 3.14159265358979323846;
+constexpr auto FOV = PI/3.;
 constexpr auto MAX_REFLECTIONS = 4;
 constexpr auto BACKGROUND_COLOR = QVector3D(0.2f, 0.7f, 0.8f);
+constexpr auto offset = 1e-3;
 
 
-QVector3D mix(const QVector3D &a, const QVector3D &b, const float k){
+
+[[nodisacrd]] auto mix(const QVector3D &a, const QVector3D &b, const float k){
     return k * a + (1 - k) * b;
 }
 
 
-QVector3D reflect(const QVector3D &incident, const QVector3D &normal) {
+[[nodisacrd]] auto reflect(const QVector3D &incident, const QVector3D &normal) {
 return incident - 2.f * normal * (QVector3D::dotProduct(incident, normal));
 }
 
 
-QVector3D refract(const QVector3D &incident, const QVector3D &normal, const float n_1, const float n_2=1.f) {
-    auto cosine_1 = - std::max(-1.f, std::min(1.f, QVector3D::dotProduct(incident, normal)));
-    if (cosine_1 < 0.f) return refract(incident, -normal, n_2, n_1);
-    auto ratio = n_2 / n_1;
-    auto cosine_2 = sqrtf(1 - ratio * ratio * (1 - cosine_1 * cosine_1));
-    return  ratio * incident + (ratio * cosine_1 - cosine_2) * normal;
+[[nodisacrd]] auto refract(const QVector3D &incident, const QVector3D &normal, const float n_1, const float n_2=1.f) {
+    const auto cosine_1 = - std::max(-1.f, std::min(1.f, QVector3D::dotProduct(incident, normal)));
+
+    if (cosine_1 >= 0.f) {
+        const auto ratio = n_2 / n_1;
+        const auto cosine_2 = sqrtf(1 - ratio * ratio * (1 - cosine_1 * cosine_1));
+        return ratio * incident + (ratio * cosine_1 - cosine_2) * normal;
+    }
+    return refract(incident, -normal, n_2, n_1);
 }
 
 
-bool scene_intersect(const Ray &ray, const std::vector<Sphere> &spheres, const std::vector<Plane> &planes, QVector3D &hit, QVector3D &normal, Material &material) {
+[[nodisacrd]] auto scene_intersect(const Ray &ray, const std::vector<Sphere> &spheres, const std::vector<Plane> &planes, QVector3D &hit, QVector3D &normal, Material &material) {
     auto spheres_dist = std::numeric_limits<float>::max();
     for (const auto & sphere : spheres) {
         auto dist_i = std::numeric_limits<float>::max();
@@ -48,16 +52,16 @@ bool scene_intersect(const Ray &ray, const std::vector<Sphere> &spheres, const s
             hit = ray.origin + ray.direction * dist_i;
             normal = (hit - sphere.center).normalized();
             if(sphere.material.isTexture()) {
-                int u = (sphere.material.img.width()-1) * (1 + atan2(normal.z(), normal.x()) / M_PI) * 0.5f;
-                int v = (sphere.material.img.height()-1) * acosf(normal.y()) / M_PI;
-                QColor tmp = sphere.material.img.pixel(u, v);
+                int u = (sphere.material.image.width()-1) * (1 + atan2(normal.z(), normal.x()) / PI) * 0.5f;
+                int v = (sphere.material.image.height()-1) * acosf(normal.y()) / PI;
+                QColor tmp = sphere.material.image.pixel(u, v);
                 material.diffuseColor = QVector3D(tmp.redF(), tmp.greenF(), tmp.blueF());
             }else {
                 material = sphere.material;
             }
         }
     }
-    float planes_dist = std::numeric_limits<float>::max();
+    auto planes_dist = std::numeric_limits<float>::max();
     for (const auto & plane : planes) {
         auto dist_i = std::numeric_limits<float>::max();
         if (plane.ray_intersect(ray, dist_i) && dist_i < spheres_dist) {
@@ -71,7 +75,7 @@ bool scene_intersect(const Ray &ray, const std::vector<Sphere> &spheres, const s
 }
 
 
-QVector3D cast_ray(const Ray &ray, const std::vector<Sphere> &spheres, const std::vector<Plane> &planes, const std::vector<Light> &lights, size_t depth=0){
+[[nodisacrd]] auto cast_ray(const Ray &ray, const std::vector<Sphere> &spheres, const std::vector<Plane> &planes, const std::vector<Light> &lights, size_t depth=0){
     QVector3D point;
     QVector3D normal;
     Material material;
@@ -79,29 +83,32 @@ QVector3D cast_ray(const Ray &ray, const std::vector<Sphere> &spheres, const std
     if (depth > MAX_REFLECTIONS || !scene_intersect(ray, spheres, planes, point, normal, material)){
         return BACKGROUND_COLOR;
     }
-    QVector3D reflect_direction = reflect(ray.direction, normal).normalized();
-    QVector3D refract_direction = refract(ray.direction, normal, material.refractiveIndex).normalized();
-    QVector3D reflect_origin = QVector3D::dotProduct(reflect_direction, normal) < 0 ? point - normal*1e-3f : point + normal*1e-3f;
-    QVector3D refract_origin = QVector3D::dotProduct(refract_direction, normal) < 0 ? point - normal*1e-3f : point + normal*1e-3f;
-    QVector3D reflect_color = cast_ray(Ray(reflect_origin, reflect_direction), spheres, planes, lights, depth + 1);
-    QVector3D refract_color = cast_ray(Ray(refract_origin, refract_direction), spheres, planes, lights, depth + 1);
+    const auto reflect_direction = reflect(ray.direction, normal).normalized();
+    const auto refract_direction = refract(ray.direction, normal, material.refractiveIndex).normalized();
+    const auto reflect_origin = QVector3D::dotProduct(reflect_direction, normal) < 0 ? point - normal*offset : point + normal*offset;
+    const auto refract_origin = QVector3D::dotProduct(refract_direction, normal) < 0 ? point - normal*offset : point + normal*offset;
+    Ray reflect_ray = {.origin = reflect_origin, .direction = reflect_direction};
+    Ray refract_ray = {.origin = refract_origin, .direction = refract_direction};
+    const auto reflect_color = cast_ray(reflect_ray, spheres, planes, lights, depth + 1);
+    const auto refract_color = cast_ray(refract_ray, spheres, planes, lights, depth + 1);
 
     auto diffuse_light_intensity = 0.f;
     auto specular_light_intensity = 0.f;
     for(auto light : lights) {
-        QVector3D light_direction = (light.position - point).normalized();
-        auto light_distance = (light.position - point).length();
-        QVector3D shadow_origin = QVector3D::dotProduct(light_direction, normal) < 0 ? point - normal*1e-3f : point + normal*1e-3f;
+        const auto light_direction = (light.position - point).normalized();
+        const auto light_distance = (light.position - point).length();
+        const auto shadow_origin = QVector3D::dotProduct(light_direction, normal) < 0 ? point - normal*offset : point + normal*offset;
         QVector3D shadow_point;
         QVector3D shadow_normal;
         Material tmpmaterial;
-        if (scene_intersect(Ray(shadow_origin, light_direction), spheres, planes, shadow_point, shadow_normal, tmpmaterial) && (shadow_point - shadow_origin).length() < light_distance)
+        Ray light_ray = {.origin = shadow_origin, .direction = light_direction};
+        if (scene_intersect(light_ray, spheres, planes, shadow_point, shadow_normal, tmpmaterial) && (shadow_point - shadow_origin).length() < light_distance)
             continue;
         diffuse_light_intensity  += light.intensity * std::max(0.f, QVector3D::dotProduct(light_direction, normal));
         specular_light_intensity += powf(std::max(0.f, QVector3D::dotProduct(reflect(light_direction, normal), ray.direction)), material.specularFactor) * light.intensity;
     }
-    auto k = std::exp(-(point-ray.origin).length()/100.f);
-    auto obj_color = material.diffuseColor * diffuse_light_intensity * material.proportion.x()
+    const auto k = std::exp(-(point-ray.origin).length()/100.f);
+    const auto obj_color = material.diffuseColor * diffuse_light_intensity * material.proportion.x()
     + QVector3D(1., 1., 1.)*specular_light_intensity * material.proportion.y()
     + reflect_color*material.proportion.z()
     + refract_color*material.proportion.w();
@@ -115,9 +122,9 @@ int main(){
     Material mirror(QVector3D(1.0f, 1.0f, 1.0f), 1425., 1.0, QVector4D(0.0f, 1.0f, 0.8f, 0.0f));
     Material glass(QVector3D(0.6f, 0.7f, 0.8f), 125., 1.5, QVector4D(0.0f, 0.5f, 0.1f, 0.8f));
     QImage mars_tex = QImage(":/textures/mars.jpg");
-    Material mars(mars_tex);
+    Material mars(std::move(mars_tex));
     QImage earth_tex = QImage(":/textures/earth.jpg");
-    Material earth(earth_tex);
+    Material earth(std::move(earth_tex));
     Material plane_mater;
 
     std::vector<Sphere> spheres;
@@ -132,19 +139,23 @@ int main(){
     planes.emplace_back(Plane(QVector3D(0,-4,0),QVector3D(0,1,0), plane_mater));
 
     std::vector<Light>  lights;
-    lights.emplace_back(Light(QVector3D( 30, 50, -25), 1.5));
-    lights.emplace_back(Light(QVector3D( 30, 20,  30), 2));
-    lights.emplace_back(Light(QVector3D(0, 20,  0), 2.5));
+    Light light_1 = {.position = QVector3D( 30, 50, -25), .intensity = 1.5f};
+    Light light_2 = {.position = QVector3D( 30, 20,  30), .intensity = 2.0f};
+    Light light_3 = {.position = QVector3D(0, 20,  0), .intensity = 2.5f};
+
+    lights.emplace_back(light_1);
+    lights.emplace_back(light_2);
+    lights.emplace_back(light_3);
 
 
     QImage result(WIDTH, HEIGHT, QImage::Format_RGB32);
     QVector3D resColor;
     for(auto y = 0; y < result.height(); y++){
         for(auto x = 0; x < result.width(); x++){
-            auto dir_x =  (x + 0.5f) -  result.width()/2.f;
-            auto dir_y = -(y + 0.5f) + result.height()/2.f;
-            auto dir_z = -result.height()/(2.f*std::tan(FOV/2.f));
-            Ray ray(QVector3D(0,0,0), QVector3D(dir_x,dir_y,dir_z).normalized());
+            const auto dir_x =  (x + 0.5f) -  result.width()/2.f;
+            const auto dir_y = -(y + 0.5f) + result.height()/2.f;
+            const auto dir_z = -result.height()/(2.f*std::tan(FOV/2.f));
+            Ray ray = {.origin = QVector3D(0,0,0), .direction = QVector3D(dir_x,dir_y,dir_z).normalized()};
             resColor = cast_ray(ray, spheres, planes,  lights);
             result.setPixel(x,y, qRgb(std::min(int(resColor.x()*255),255), std::min(int(resColor.y()*255),255),std::min(int(resColor.z()*255), 255)));
         }
