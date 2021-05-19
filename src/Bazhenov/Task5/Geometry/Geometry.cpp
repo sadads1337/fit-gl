7128f144600a614377226e0fb00393ee6432857d
Artyom Bazhenov <tema.bajen@gmail.com>
2021-05-19 17:48:49 +0700

    Task 5 renders empty space

---
+++ src/Bazhenov/Task5/Geometry/Geometry.cpp
@@ -0,0 +1,43 @@
+#include "Geometry.hpp"
+
+namespace Bazhenov {
+
+std::optional<float> sphere_intersection(const Ray &ray,
+                                         const QVector3D &center,
+                                         const float radius) {
+  const QVector3D to_sphere = ray.origin - center;
+  const auto dir_projection = dot(ray.direction, to_sphere);
+  const auto tmp =
+      sqr(dir_projection) - (to_sphere.lengthSquared() - sqr(radius));
+  if (tmp < 0) {
+    return std::nullopt;
+  }
+
+  const auto sqrt_tmp = sqrtf(tmp);
+  const auto t1 = -dir_projection - sqrt_tmp;
+  if (t1 > EPSILON) {
+    return t1;
+  }
+
+  const auto t2 = -dir_projection + sqrt_tmp;
+  if (t2 > EPSILON) {
+    return t2;
+  }
+
+  return std::nullopt;
+}
+
+std::optional<float> plane_intersection(const Ray &ray, const QVector3D &p0,
+                                        const QVector3D &n) {
+  const auto denom = dot(ray.direction, n);
+  if (denom > -EPSILON && denom < EPSILON) {
+    return std::nullopt;
+  }
+  const auto res = dot(p0 - ray.origin, n) / denom;
+  if (res < EPSILON) {
+    return std::nullopt;
+  }
+  return res;
+}
+
+}
---
+++ src/Bazhenov/Task5/Lighting/Light.cpp
@@ -0,0 +1,24 @@
+#include "Light.hpp"
+#include "../Geometry/Geometry.hpp"
+
+namespace Bazhenov {
+
+QVector3D ambient(const float strength, const QVector3D light_color) {
+  return strength * light_color;
+}
+
+QVector3D diffuse(const QVector3D normal, const QVector3D light_dir,
+                  const QVector3D light_color) {
+  const auto diff = std::max(dot(normal, light_dir), 0.0F);
+  return diff * light_color;
+}
+
+QVector3D specular(const QVector3D normal, const QVector3D light_dir,
+                   const QVector3D view_dir, const float strength,
+                   const float power, const QVector3D light_color) {
+  const auto ref_dir = reflect(-light_dir, normal);
+  const auto spec = std::pow(std::max(dot(view_dir, ref_dir), 0.0F), power);
+  return strength * spec * light_color;
+}
+
+}
