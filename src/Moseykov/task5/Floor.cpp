898d8c5be46e2e58555a2654e389ecd8bcdfedd3
Ilya Makarov <i.o.makarov@2gis.ru>
2021-05-24 10:08:45 +0700

    Code-Style

--- src/Moseykov/task5/Floor.cpp
+++ src/Moseykov/task5/Floor.cpp
@@ -3,33 +3,34 @@
 
 constexpr auto precision = 1e-6;
 
-std::optional<HitRecord> Floor::hit(const Ray& r) const{
-    const auto dir_y = QVector3D::dotProduct(r.direction(), norm);
-    if(std::abs(dir_y) <= precision) { return std::nullopt; }
-    const QVector3D dist = pos - r.origin();
-    const auto t = QVector3D::dotProduct(dist, norm) / dir_y;
-    if(t < precision) { return std::nullopt; }
-    HitRecord hit;
-    hit.t = t;
-    hit.norm = norm;
-    hit.p = r.at(t);
-    constexpr auto cell_size = 0.4F;
-    const auto c = static_cast<int>(std::floor(hit.p.x() / cell_size)) +
-                       static_cast<int>(std::floor(hit.p.z() / cell_size));
-    if(c % 2 == 0){
-         hit.color = QVector3D(1.0f, 1.0f, 1.0f);
-         hit.material.albedo = QVector4D(0.6f, 0.3f, 0.1f, 0.0f);
-         hit.material.specularFactor = 50;
-         hit.material.refractiveIndex = 1;
-        }
-    else{
-         hit.color = QVector3D(0.0f, 0.9f, 0.1f);
-         hit.material.albedo = QVector4D(0.6f, 0.3f, 0.1f, 0.0f);
-         hit.material.specularFactor = 50;
-         hit.material.refractiveIndex = 1;
-        }
+std::optional<HitRecord> Floor::hit(const Ray &r) const {
+  const auto dir_y = QVector3D::dotProduct(r.direction(), norm);
+  if (std::abs(dir_y) <= precision) {
+    return std::nullopt;
+  }
+  const QVector3D dist = pos - r.origin();
+  const auto t = QVector3D::dotProduct(dist, norm) / dir_y;
+  if (t < precision) {
+    return std::nullopt;
+  }
+  HitRecord hit;
+  hit.t = t;
+  hit.norm = norm;
+  hit.p = r.at(t);
+  constexpr auto cell_size = 0.4F;
+  const auto c = static_cast<int>(std::floor(hit.p.x() / cell_size)) +
+                 static_cast<int>(std::floor(hit.p.z() / cell_size));
+  if (c % 2 == 0) {
+    hit.color = QVector3D(1.0f, 1.0f, 1.0f);
+    hit.material.albedo = QVector4D(0.6f, 0.3f, 0.1f, 0.0f);
+    hit.material.specularFactor = 50;
+    hit.material.refractiveIndex = 1;
+  } else {
+    hit.color = QVector3D(0.0f, 0.9f, 0.1f);
+    hit.material.albedo = QVector4D(0.6f, 0.3f, 0.1f, 0.0f);
+    hit.material.specularFactor = 50;
+    hit.material.refractiveIndex = 1;
+  }
 
-    return hit;
-
-
+  return hit;
 }
--- src/Moseykov/task5/Sphere.cpp
+++ src/Moseykov/task5/Sphere.cpp
@@ -1,23 +1,25 @@
 #include "Sphere.h"
 #include "QtMath"
 
-std::optional<HitRecord> Sphere::hit(const Ray& r) const {
+std::optional<HitRecord> Sphere::hit(const Ray &r) const {
   HitRecord hit;
   QVector3D oc = r.origin() - center;
   const auto a = r.direction().lengthSquared();
   const auto b_hf = QVector3D::dotProduct(oc, r.direction());
-  const auto c = oc.lengthSquared() - radius*radius;
-  const auto discriminant = b_hf*b_hf - a*c;
-  if(discriminant < 0) { return std::nullopt;}
+  const auto c = oc.lengthSquared() - radius * radius;
+  const auto discriminant = b_hf * b_hf - a * c;
+  if (discriminant < 0) {
+    return std::nullopt;
+  }
   const auto sqrtd = sqrt(discriminant);
 
   // Find the nearest root that lies in the acceptable range.
   auto root = (-b_hf - sqrtd) / a;
   if (root < 0 || INFINITY < root) {
-      root = (-b_hf + sqrtd) / a;
-      if(root < 0 || INFINITY < root) {
-          return std::nullopt;
-      }
+    root = (-b_hf + sqrtd) / a;
+    if (root < 0 || INFINITY < root) {
+      return std::nullopt;
+    }
   }
   hit.t = root;
   hit.p = r.at(hit.t);
