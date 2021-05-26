ab9690bdcf7ba1e379840bda10b40a6f57f7e556
void <49158033+Narick09@users.noreply.github.com>
2021-05-26 14:49:34 +0700

    Update cube.hpp

--- src/Voitov/Task2/cube.hpp
+++ src/Voitov/Task2/cube.hpp
@@ -7,7 +7,7 @@ class Cube final: public FigureInformation {
 public:
     void init(GLfloat width, GLuint N);
 private:
-    GLfloat width = 1.0;
+    GLfloat half_width = 1.0;
     GLuint num_of_vertixes = 8;
 };
 }
