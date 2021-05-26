f3c3e03d18340e052ef508aa4aa9276eabafdeeb
void <49158033+Narick09@users.noreply.github.com>
2021-05-26 16:38:35 +0700

    Update figure.cpp

--- src/Voitov/Task3/figure.cpp
+++ src/Voitov/Task3/figure.cpp
@@ -6,19 +6,4 @@ VertexData::VertexData(QVector3D position, QVector3D normal){
     this->position = position;
     this->norm = normal;
 }
-
-//QVector<VertexData> figure::getVertexes(){
-//    return this->gVertexes;
-//}
-//QVector<GLushort> figure::getIndexes(){
-//    return this->gIndexes;
-//}
-
-//QVector3D figure::getRotationAxis(){
-//    return this->rotationAxis;
-//}
-
-//QVector3D figure::getCenter(){
-//    return this->center;
-//}
 }
